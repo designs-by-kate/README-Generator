186b3125f72091b0b0575eb69c3ce28942d32810
designs-by-kate <kate_diet@hotmail.com>
2024-02-16 18:49:13 +0000

    Added content to generateMarkdown.js file and implement logic in index.js to prompt users for information using inquirer

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# README-Generator
+# 
+  ![License Badge](https://img.shields.io/badge/license-MIT-brightgreen)
+  
+  ## Description
+  
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  
+  ## Usage
+  
+  
+  ## License
+  This project is licensed under the MIT License.
+  
+  ## Contributing
+  
+
+  ## Tests
+  
+  
+  ## Questions
+  For any questions or feedback, feel free to contact me:
+  - GitHub: [](https://github.com/)
+  - Email: 
--- index.js
+++ index.js
@@ -5,17 +5,75 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project: What was your motivation? Why did you build this project? What problem does it solve?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache', 'GNU', 'BSD', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide any test instructions:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (For questions and feedback)',
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    const outputPath = path.join(process.cwd(), fileName);
+
+    fs.writeFile(outputPath, data, (err) => {
+        if (err) {
+            console.error('Error writing README file:', err);
+        } else {
+            console.log(`${fileName} generated successfully!`);
+        }
+    });
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((answers) => {
+        const markdownContent = generateMarkdown(answers);
+        writeToFile('README.md', markdownContent);
+    });
 }
+
 
 // function call to initialize program
 init();
